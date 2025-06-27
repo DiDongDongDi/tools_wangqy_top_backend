0d4affc2a9c2125b7b646f8cf703b7788de2258b
kodyqywang <kodyqywang@tencent.com>
2025-06-27 13:17:12 +0800

    Revert "更新settings.py，将数据库引擎更改为MySQL，并配置相关连接参数，包括数据库名称、用户、密码、主机和端口。"
    This reverts commit 09c6abe661d3a327c86a6ebb7dc4d04520056618.

--- src/src/settings.py
+++ src/src/settings.py
@@ -76,15 +76,8 @@ WSGI_APPLICATION = "src.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "django_db",
-        "USER": "django_user",
-        "PASSWORD": "django_password",
-        "HOST": "localhost",
-        "PORT": "3306",
-        "OPTIONS": {
-            "charset": "utf8mb4",
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
